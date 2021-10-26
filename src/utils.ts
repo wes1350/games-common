41323409fe15f121f354fc137e84e97786b8a5fc
Wesley Runnels <wrunnels@mit.edu>
2021-10-25 22:58:48 -0400

    Update GenerateId function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
-export const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+import _ from "lodash";
+
+export const Sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const HEX_CHARSET = "abcdef0123456789";
+export const ALPHANUMERIC_NONVOWEL =
+    "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ0123456789";
+
+export const GenerateId = (charSet: string, length: number = 16): string => {
+    return _.range(length)
+        .map(() => charSet.charAt(Math.floor(Math.random() * charSet.length)))
+        .join("");
 };
